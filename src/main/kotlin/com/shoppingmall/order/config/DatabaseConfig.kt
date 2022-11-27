53fa4153b0f5520aeca5b81040988ea25a7b721f
juneyoung-jo@github.com <juneyoung-jo@github.com>
2022-11-27 12:05:23 +0900

    feat : vertx client 수정
    - jdbc -> mysql

--- src/main/kotlin/com/shoppingmall/order/config/DatabaseConfig.kt
+++ src/main/kotlin/com/shoppingmall/order/config/DatabaseConfig.kt
@@ -1,15 +1,12 @@
 package com.shoppingmall.order.config
 
 import io.vertx.core.Vertx
-import io.vertx.jdbcclient.JDBCConnectOptions
-import io.vertx.jdbcclient.JDBCPool
+import io.vertx.mysqlclient.MySQLConnectOptions
+import io.vertx.mysqlclient.MySQLPool
 import io.vertx.sqlclient.Pool
 import io.vertx.sqlclient.PoolOptions
 import io.vertx.sqlclient.SqlConnectOptions
-import org.hibernate.internal.util.config.ConfigurationHelper
 import org.hibernate.reactive.pool.impl.DefaultSqlClientPool
-import org.hibernate.reactive.pool.impl.DefaultSqlClientPoolConfiguration
-import org.hibernate.reactive.provider.Settings
 import java.net.URI
 
 class MysqlConnectionPool : DefaultSqlClientPool() {
@@ -19,54 +16,53 @@ class MysqlConnectionPool : DefaultSqlClientPool() {
         poolOptions: PoolOptions,
         vertx: Vertx
     ): Pool {
-        return JDBCPool.pool(
+        return MySQLPool.pool(
             vertx,
-            JDBCConnectOptions()
-                .setJdbcUrl(connectOptions.host)
+            MySQLConnectOptions()
+                .setHost(connectOptions.host)
                 .setUser(connectOptions.user)
                 .setPassword(connectOptions.password)
-                .setDatabase("mysql"),
+                .setDatabase(connectOptions.database),
             poolOptions
         )
     }
 }
 
-class VertxMySqlonnectionPoolConfiguration : DefaultSqlClientPoolConfiguration() {
-    private lateinit var user: String
-    private var pass: String? = null
-    private var cacheMaxSize: Int? = null
-    private var sqlLimit: Int? = null
-    override fun connectOptions(uri: URI): SqlConnectOptions {
-        if (uri.scheme == "mysql") {
-            return SqlConnectOptions()
-                .setHost("jdbc:$uri")
-                .setUser(user)
-                .setDatabase("mysql")
-                .apply {
-                    pass?.let { password = it }
-                    //enable the prepared statement cache by default
-                    cachePreparedStatements = true
-
-                    cacheMaxSize?.run {
-                        if (this <= 0) {
-                            cachePreparedStatements = false
-                        } else {
-                            preparedStatementCacheMaxSize = this
-                        }
-                    }
-
-                    sqlLimit?.run { setPreparedStatementCacheSqlLimit(this) }
-                }
-        }
-
-        return super.connectOptions(uri)
-    }
-
-    override fun configure(configuration: MutableMap<Any?, Any?>) {
-        user = ConfigurationHelper.getString(Settings.USER, configuration)
-        pass = ConfigurationHelper.getString(Settings.PASS, configuration)
-        cacheMaxSize = ConfigurationHelper.getInteger(Settings.PREPARED_STATEMENT_CACHE_MAX_SIZE, configuration)
-        sqlLimit = ConfigurationHelper.getInteger(Settings.PREPARED_STATEMENT_CACHE_SQL_LIMIT, configuration)
-        super.configure(configuration)
-    }
-}
+//class VertxMySqlonnectionPoolConfiguration : DefaultSqlClientPoolConfiguration() {
+//    private lateinit var user: String
+//    private var pass: String? = null
+//    private var cacheMaxSize: Int? = null
+//    private var sqlLimit: Int? = null
+//    override fun connectOptions(uri: URI): SqlConnectOptions {
+//        if (uri.scheme == "mysql") {
+//            val apply = SqlConnectOptions()
+//                .setUser(user)
+//                .apply {
+//                    pass?.let { password = it }
+//                    //enable the prepared statement cache by default
+//                    cachePreparedStatements = true
+//
+//                    cacheMaxSize?.run {
+//                        if (this <= 0) {
+//                            cachePreparedStatements = false
+//                        } else {
+//                            preparedStatementCacheMaxSize = this
+//                        }
+//                    }
+//
+//                    sqlLimit?.run { setPreparedStatementCacheSqlLimit(this) }
+//                }
+//            return apply
+//        }
+//
+//        return super.connectOptions(uri)
+//    }
+//
+//    override fun configure(configuration: MutableMap<Any?, Any?>) {
+//        user = ConfigurationHelper.getString(Settings.USER, configuration)
+//        pass = ConfigurationHelper.getString(Settings.PASS, configuration)
+//        cacheMaxSize = ConfigurationHelper.getInteger(Settings.PREPARED_STATEMENT_CACHE_MAX_SIZE, configuration)
+//        sqlLimit = ConfigurationHelper.getInteger(Settings.PREPARED_STATEMENT_CACHE_SQL_LIMIT, configuration)
+//        super.configure(configuration)
+//    }
+//}
